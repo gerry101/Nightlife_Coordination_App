90067006cc68e7db1ec1bf57a52b6983c766b4a1
Gerry B. Migwi <migwimwash@gmail.com>
2017-07-03 18:49:29 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    express = require('express'),
+    app = express();
+
+var indexRoutes = require('./routes/index');
+
+mongoose.connect('mongodb://localhost/nightlife');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+app.use(indexRoutes);
+
+app.set('view engine', 'ejs');
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+   console.log('Server running...'); 
+});
